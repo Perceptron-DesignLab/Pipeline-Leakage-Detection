15538efaa9a9fc5736333d25f3e79be8d46d0127
Perceptron-DesignLab <66838444+Perceptron-DesignLab@users.noreply.github.com>
2020-06-17 12:23:48 +0530

    Update test_server_module.py

--- Test/test_server_module.py
+++ Test/test_server_module.py
@@ -6,13 +6,13 @@ class test_cloud_manager(unittest.TestCase) :
         cloud_manager_1=cloud_manager()
         cloud_manager_1.create_lookup_table()
         cloud_manager_1.get_water_sensor_data(560,1)
-        self.assertEqual(cloud_manager_1.process_water_sensor_data(),"Fully Submerged")
+        self.assertEqual(cloud_manager_1.process_water_sensor_data(),"Fully Submerged.")
 
     def test_process_ir_sensor_data(self) :
         cloud_manager_1=cloud_manager()
         cloud_manager_1.create_lookup_table()
         cloud_manager_1.get_ir_sensor_data(1,1)
-        self.assertEqual(cloud_manager_1.process_ir_sensor_data(),"obstacles present")
+        self.assertEqual(cloud_manager_1.process_ir_sensor_data(),"Obstacles Present")
     
 if __name__ == '__main__':
     unittest.main()
